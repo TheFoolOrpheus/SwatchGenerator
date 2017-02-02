bf7331b430bf9424d6306dde8cc7809d88052ee2
TheFoolOrpheus <16de0991@opayq.com>
2017-02-02 04:02:14 -0500

    Added current project classes
    First java class of this particular project.

---
+++ ColorCreator/src/colorCreator/Creator.java
@@ -0,0 +1,16 @@
+package colorCreator;
+
+/**
+ *
+ * @author Ashlee Daniel
+ * 
+ */
+
+public class Creator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
