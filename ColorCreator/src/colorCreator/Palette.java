4c216dbf53adb1a51efbe0b889902f92ace313d0
TheFoolOrpheus <16de0991@opayq.com>
2017-02-06 21:24:55 -0500

    GridBagLayout added
    I need to fix the buttons, they show up in the middle and they're too
    big. They need to run across the top, and then another set will be
    running along the bottom. The middle of the screen is left for the
    swatch itself. Also, I need to make the screen non-resizable.

--- ColorCreator/src/colorCreator/Palette.java
+++ ColorCreator/src/colorCreator/Palette.java
@@ -1,5 +1,8 @@
 package colorCreator;
 
+import java.awt.ComponentOrientation;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -31,22 +34,71 @@ public class Palette extends JFrame {
 	
 	private void paletteWindow(){
 		
+		
+		/*
+		 * Not sure why, I need to read up on GridBagLayout to
+		 * see what the purpose of doing this kind of thing is.
+		 */
+		boolean shouldFill = true;
+		boolean shouldWeightX = true;
+		
+		/*
+		 * Creating the frame, which I might have to make non-resizable.
+		 * The layout is GridBag.
+		 */
 		setTitle("Swatch Creator");
 		setSize(1000,1000);
 		setLocationRelativeTo(null);
 		setAlwaysOnTop(true);
 		setVisible(true);
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
-		
-		//Testing
-		JButton newButton = new JButton("Test Button");
-		newButton.setActionCommand("Test Button");
-		newButton.addActionListener(new ActionListener() {
+		setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		if(shouldFill){
+			constraints.fill = GridBagConstraints.HORIZONTAL;
+		}
+				
+		/*
+		 * Creating the monochrome button. Should pass isPastel and
+		 * paletteNumber to the monochrome class.
+		 * 
+		 */
+		JButton monochrome = new JButton("Monochrome");
+		monochrome.setActionCommand("Monochrome");
+		if(shouldWeightX){
+			constraints.weightx = 0.5;
+		}
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.gridwidth = 1;
+		//constraints.fill = GridBagConstraints.HORIZONTAL;
+		monochrome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
 				isPastel();
 			}
 		});
-		add(newButton);
+		add(monochrome, constraints);
+		
+		/*
+		 * Creating a adjacent color button. Should pass isPastel and
+		 *  paletteNumber to the adjacent class.
+		 */
+		JButton adjacent = new JButton("Adjacent");
+		adjacent.setActionCommand("Adjacent");
+		if(shouldWeightX){
+			constraints.weightx = 0.5;
+		}
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		constraints.gridwidth = GridBagConstraints.RELATIVE;
+		//constraints.fill = GridBagConstraints.HORIZONTAL;
+		adjacent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				isPastel();
+			}
+		});
+		add(adjacent, constraints);
 	}
 	
 	
@@ -60,15 +112,9 @@ public class Palette extends JFrame {
 	private boolean isPastel(){
 		
 		JFrame frame = new JFrame();
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setAlwaysOnTop(true);
-		/*JFrame frame2 = new JFrame();
-		frame2.setVisible(true);
-		frame2.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		frame2.setLocationRelativeTo(null);
-		frame2.setAlwaysOnTop(true);*/
 		boolean pastel = false;
 		
 		int n = JOptionPane.showConfirmDialog(frame, "Are you creating a pastel palette?",
@@ -101,7 +147,10 @@ public class Palette extends JFrame {
 		return pastel;
 	}
 	
-	
+	private int paletteNumber(){
+		
+		return 0;
+	}
 	
 
 }
